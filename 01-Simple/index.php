6269986c60b32abc8f20970a1bdc1bbb56fe44a4
tejasvaidya01 <tejasvaidya01@users.noreply.github.com>
2017-07-02 19:48:51 +0530

    Documentation added

--- 01-Simple/index.php
+++ 01-Simple/index.php
@@ -1,9 +1,19 @@
 <?php
 // index.php 20150101 - 20170302
 // Copyright (C) 2015-2017 Mark Constable <markc@renta.net> (AGPL-3.0)
+// Documentation added by Tejas Vaidya
 
+/**
+*	Class object is created and printed with echo
+*/
 echo new class
 {
+    /**
+    *	These are private variable arrays($in, $out and $nav1), Only accessible within the class.
+    *	$in defines request parameters with default values.
+    *	$out defines various html regions.
+    *	$nav1 defines navigation menu links with request parameters
+    */
     private
     $in = [
         'm'     => 'home',      // Method action
@@ -21,6 +31,13 @@ echo new class
         ['Contact', '?m=contact'],
     ];
 
+    /**
+    *	This function is called when the class object is created.
+    *	It defines variables and methods.
+    *	method_exists() checks if in['m'] is exists within this class.
+    *	If request parameter does not exists then home output is set by default
+    *	Also checks if output methods exists and call those, else set default values from $out
+    */
     public function __construct()
     {
         $this->in['m'] = $_REQUEST['m'] ?? $this->in['m'];
@@ -30,11 +47,20 @@ echo new class
             $this->out[$k] = method_exists($this, $k) ? $this->$k() : $v;
     }
 
+    /**
+    *	This function is called after __construct function.
+    *	It converts the output to the string.
+    *	It calls private function html() of the same class.
+    */
     public function __toString() : string
     {
         return $this->html();
     }
 
+    /**
+    *	Navigation menu is created.
+    *	For each menu in $nav a link is created and then joined together.
+    */
     private function nav1() : string
     {
         return '
@@ -45,6 +71,9 @@ echo new class
       </nav>';
     }
 
+    /**
+    *	Returns output for the header region.
+    */
     private function head() : string
     {
         return '
@@ -53,6 +82,9 @@ echo new class
     </header>';
     }
 
+    /**
+    *	Returns output for the main region.
+    */
     private function main() : string
     {
         return '
@@ -60,6 +92,9 @@ echo new class
     </main>';
     }
 
+    /**
+    *	Returns output for the footer region.
+    */
     private function foot() : string
     {
         return '
@@ -68,6 +103,11 @@ echo new class
     </footer>';
     }
 
+    /**
+    *	This converts the output into a string.
+	*	extract creates local variables for for every entry in $out array.
+	*	HTML page is created here with all regions.
+    */
     private function html() : string
     {
         extract($this->out, EXTR_SKIP);
@@ -84,6 +124,10 @@ echo new class
 ';
     }
 
+    /**
+    *	Creates the content for each page of the navigation.
+    *	Called from the __construct to set output of the main region
+    */
     private function home() { return '<h2>Home Page</h2><p>Lorem ipsum home.</p>'; }
     private function about() { return '<h2>About Page</h2><p>Lorem ipsum about.</p>'; }
     private function contact() { return '<h2>Contact Page</h2><p>Lorem ipsum contact.</p>'; }
